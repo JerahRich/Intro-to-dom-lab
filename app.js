69104f9a09c02f2fe1f70219e27cbec202d9ca45
JerahRich <jerahrichens@gmail.com>
2025-12-09 13:31:06 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+/*-------------------------------- Constants --------------------------------*/
+
+const buttons = document.querySelectorAll('.button');
+
+
+/*-------------------------------- Variables --------------------------------*/
+
+/*------------------------ Cached Element References ------------------------*/
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+buttons.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    // This log is for testing purposes to verify we're getting the correct value
+    console.log(event.target.innerText);
+    // Future logic to capture the button's value would go here...
+  });
+});
+
+
+/*-------------------------------- Functions --------------------------------*/
